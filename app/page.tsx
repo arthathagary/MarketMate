0f5d92e444dc5053e9ac3d8432b07d41d78165d5
Arsath8597 <arsath8597@gmail.com>
2023-11-26 19:40:13 +0530

    add popup window on item add page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import Image from 'next/image'
 import Login from './login/page'
 import DataTable from '../components/Item/DataTable'
 import Card from '../components/Item/add'
+import Popup from '../components/Item/popup'
 
 export default function Home() {
   return (
     <>
     {/* <Login /> */}
     {/* <DataTable/> */}
-    <Card/>
-    {/* <ModeToggle /> */}
+    {/* <Card/> */}
+    <ModeToggle />
+    <Popup/>
     </>
   )
 }
